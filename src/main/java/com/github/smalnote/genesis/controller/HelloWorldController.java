b90486cada9959ecf44804087a93328a9ad53667
smalnote <smalnote@gmail.com>
2018-02-14 22:28:54 +0800

    add ehcache

---
+++ src/main/java/com/github/smalnote/genesis/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.github.smalnote.genesis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "/delta.io/index.html";
+	}
+	
+}
--- src/main/java/com/github/smalnote/genesis/controller/RequestExceptionHandler.java
+++ src/main/java/com/github/smalnote/genesis/controller/RequestExceptionHandler.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
