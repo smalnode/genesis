35a1fa07b7c7be72f8be9ea19ea2e62d6a0eaecd
smalnode <smalnode@gmail.com>
2018-05-19 11:54:58 +0800

    add TestGreeting

---
+++ src/main/test/com/github/smalnote/genesis/controller/TestGreeting.java
@@ -0,0 +1,37 @@
+package com.github.smalnote.genesis.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringJUnitWebConfig(locations = "classpath:genesis.spring.xml")
+public class TestGreeting {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+    
+    @Test
+    public void test() throws Exception {
+    	this.mockMvc.perform(get("/greeting")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+            .andExpect(status().isOk());
+    }
+
+}
